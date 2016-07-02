cb31935b8cf3ecad9ee0b3abfb26b5600c3b59e0
Ryan Rowland <rrowland@twilio.com>
2016-07-02 16:31:08 -0700

    Added websocket server

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 'use strict';
 
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const WebSocket = require('websocket');
+
+var http = require('http');
+var server = http.createServer(function(request, response) {
+  response.writeHead(404);
+  response.end();
+});
+server.listen(8080);
 
 const app = new express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+const ws = new WebSocket.server({
+  httpServer: server
+});
+
+const connections = new Set();
+
+ws.on('request', (req) => {
+  const conn = req.accept();
+  connections.add(conn);
+
+  conn.on('close', () => {
+    connections.delete(conn);
+  });
+});
+
+function notifyClients(from, status) {
+  connections.forEach(conn => {
+    conn.sendUTF(JSON.stringify({ from, status }));
+  });
+}
 
 app.post('/test', (req, res) => {
   const body = req.body.Body.toUpperCase();
@@ -14,21 +43,23 @@ app.post('/test', (req, res) => {
   switch(body) {
     case 'IN':
       console.info(`${req.body.From} responded with: ${req.body.Body.toUpperCase()}`);
+      notifyClients(req.body.From, 'in');
       response = 'Thanks! See you then!';
       break;
     case 'OUT':
       console.info(`${req.body.From} responded with: ${req.body.Body.toUpperCase()}`);
+      notifyClients(req.body.From, 'out');
       response = 'That\'s too bad! Maybe next time.';
       break;
     default:
       console.info(`Got an unrecognized response from ${req.body.From}`);
+      notifyClients(req.body.From);
       response = 'Sorry, I didn\'t understand that. Please respond with IN or OUT.';
       break;
   }
 
   res.setHeader('Content-Type', 'text/plain');
-  console.info('response:', response);
   res.status(200).send(response);
 });
 
-app.listen(process.env.PORT || 3030);
+app.listen(process.env.PORT || 80);
